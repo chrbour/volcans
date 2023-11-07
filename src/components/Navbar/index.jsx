bda4d5c5ebe00cca06be03fc7d244deecb65cb5d
chrbour <108238838+chrbour@users.noreply.github.com>
2023-11-07 22:04:19 +0100

    feat: Create Navbar

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Navbar(){
+    return(
+        <div >
+            <div className="menuAccueil">
+                <button type="submit">Voir les photos</button>
+                <button>Ajouter contribution</button>
+                <button>Connexion</button>
+                <button>Inscription</button>
+            </div>
+        </div>
+    )
+}
--- src/pages/Accueil/index.jsx
+++ src/pages/Accueil/index.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Banner from '../../components/Banner';
+import Navbar from "../../components/Navbar";
 
 function Accueil(){
     return(
         <div>
             <Banner/>
+            <Navbar/>
             <h1 style={{color: "white"}}>Accueil</h1>
         </div>
     )
