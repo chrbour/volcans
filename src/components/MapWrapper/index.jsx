497ac3ae30062f11da13319700cdc8b3eb1a0e13
chrbour <108238838+chrbour@users.noreply.github.com>
2024-07-21 17:59:51 +0200

    Create images and map in Details Page

---
+++ src/components/MapWrapper/index.jsx
@@ -0,0 +1,54 @@
+import React, {useState, useEffect} from 'react';
+import {MapContainer , TileLayer, Marker, Popup, useMap} from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import "leaflet-geosearch/dist/geosearch.css";
+import { EsriProvider } from "leaflet-geosearch";
+import icon from "../../utils/leaflet-icon";
+
+let first = 0;
+function MapWrapper({name}){
+    let lat = 48.856667;
+    let lng = 2.351944;
+    const [position, setPosition] = useState([lat, lng])
+    function LocationMarker() {
+        const map = useMap();
+        useEffect(() => {
+          const provider = new EsriProvider();
+          provider.search({query:name})
+              .then((value)=>{
+                  lat = value[0].y;
+                  lng = value[0].x;
+                  map.flyTo([lat,lng],map.getZoom());
+                  if (first === 0){
+                    setPosition([lat, lng]);
+                    first++
+                  } 
+              })
+              .catch((error=>console.log(error)));
+        }, [])
+        return position === null ? null : (
+          <Marker position={position} icon={icon}>
+            <Popup>{name}</Popup>
+          </Marker>
+        )
+      }
+      function recentrer(){
+        setPosition([lat,lng]);
+      }
+    return (
+        <>
+            <div id = 'mapid' style = {{marginTop: 40}}>
+                <MapContainer id='maCarte' center={position} zoom={13} scrollWheelZoom={true}>
+                    <TileLayer
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    <LocationMarker />
+                </MapContainer>
+                <button onClick = {recentrer}>Recentrer</button>
+            </div>
+        </>
+    )
+}
+
+export default MapWrapper;
--- src/pages/Description/index.jsx
+++ src/pages/Description/index.jsx
@@ -3,15 +3,19 @@ import { ConnectedContext } from "../../utils/context/ConnectedProvider";
 import { VolcanoContext } from '../../utils/context/VolcanoProvider';
 import { useNavigate } from 'react-router-dom';
 import { Carousel} from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import Pictogram from '../../components/Pictogram';
+import Navbar from '../../components/Navbar';
+import MapWrapper from '../../components/MapWrapper'
 
 function Description(){
     const {connected} = useContext(ConnectedContext);
     const {volcanoId} = useContext(VolcanoContext);
     const navigate = useNavigate();
-    const [volcan, updateVolcan] = useState([]);
+    const [volcan, updateVolcan] = useState('');
     const [images, updateImages] = useState([]);
-
+    const [adress, updateAdress] = useState('');
+    
     useEffect(() => {
         fetch ('http://localhost:3000/api/volcans/')
         .then((res) => {
@@ -19,23 +23,41 @@ function Description(){
                 return res.json()
             }
         })
-        .then ((value) => {console.log(value.volcans[0]._id," ",volcanoId);
+        .then ((value) => {
             let volcano = value.volcans.filter((e) => e._id === volcanoId);
             updateVolcan(volcano[0].name);
             updateImages(volcano[0].imagesUrl);
-            console.log(volcano[0].imagesUrl);
+            updateAdress('https://fr.wikipedia.org/wiki/'+ volcano[0].name);
         })
         .catch ((err) => {
             console.log('erreur ',err);
             navigate('/Error');
         })
-    },[])
+    },[volcanoId,navigate,volcan]);
     return (
         <>
             <Pictogram connected = {connected}/>
             <h1>{volcan}</h1>
-            {images.map((e,index) => <img src= {e} alt = {index} style = {{width: 300}}></img>)}
-
+            <Navbar />
+            <div style = {{display: 'flex'}}>
+                <div>
+                    <Carousel className = "crsl">
+                        {images.map((e, index) =>{
+                            return (
+                                <div>
+                                    <img src= {e} alt = {index} ></img>
+                                    <p className = 'legend' key={index}>Image n°{index+1}</p>
+                                </div>
+                                )}
+                            )}  
+                    </Carousel>
+                    <h3>c'est un test</h3>
+                    <form action={adress} target = "_blank">
+                        <button type='submit' >BOUTON</button>
+                    </form>
+                </div>
+                <MapWrapper name={volcan}/>
+            </div>
         </>
     )
 }
