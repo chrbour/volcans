1e86d90954432c0787097b3559ce2d63497ec82f
chrbour <108238838+chrbour@users.noreply.github.com>
2026-03-04 18:47:12 +0100

    feat: only horizontals pictures accepted

--- src/pages/Contribution/index.jsx
+++ src/pages/Contribution/index.jsx
@@ -2,28 +2,41 @@ import { useState, useContext } from 'react';
 import {useNavigate} from 'react-router-dom';
 import Banner from '../../components/Banner';
 import Pictogram from '../../components/Pictogram';
-import typeVolcan from '../../assets/EruptionType.jpg';
 import { ConnectedContext } from '../../utils/context/ConnectedProvider';
 
 function Contribution(){
     const {connected} = useContext(ConnectedContext);
-    const [format, setFormat] = useState('DMS');
     const [pictures, setPictures] = useState([]);
+    const [checked, setChecked] = useState(false);
     const navigate = useNavigate();
+
     const previewImage = (e) => {
         let fileList = [];
-        for (let i=0; i < e.target.files.length; i++){
-        fileList.push(e.target.files[i])
+        setPictures([]);
+        if(e.target.files.length > 6){
+            window.alert("Vous ne pouvez pas ajouter plus de 6 images.");
+            return;
         }
-        setPictures(fileList);
+        for (let i=0; i < e.target.files.length; i++){
+            const img = new Image();
+            img.src = URL.createObjectURL(e.target.files[i]);
+            img.onload = () => {
+                if(img.width > img.height){
+                    fileList.push(e.target.files[i]);
+                    setPictures(fileList);
+                }
+            }
+        }
     }
     const goToAccueil = () => {
         navigate("/Accueil/");
     }
 
     const testingName = (e) => {
-        if (e.target.value === '') return;
-        const name = e.target.value.toLowerCase();
+        if (e.target.value === '') {
+            setChecked(false);
+            return;
+        }const name = e.target.value.toLowerCase();
         const mots = name.split(' ');
         if (mots[0] ==='le' || mots[0] === 'la' || mots[0] === 'les' || mots[0] === 'l\''){
             mots.shift();
@@ -55,14 +68,7 @@ function Contribution(){
                 testExistItem = pageId;
                 const coords = pages[pageId].coordinates[0];
                 e.target.value = pages[pageId].title;
-                console.log("PAGE 1 :");
-                console.log("pages:", pages);
-                console.log("pageId:", pageId);
-                console.log("coordinates:", coords);
-                console.log("Latitude:", coords.lat);
-                console.log("Longitude:", coords.lon);
                 http = "https://fr.wikipedia.org/w/api.php?action=query&generator=search&gsrsearch=" + e.target.value.split(' ').join('%20') + "%20volcano&prop=coordinates&redirects=1&format=json&origin=*";
-                console.log("http : ",http);
                 fetch(http)
                     .then((res) => {
                         if (res.ok){
@@ -72,27 +78,45 @@ function Contribution(){
                     .then((data) => {   
                         const pages = data.query.pages;
                         const pageId = Object.keys(pages);
-                        
-                        console.log("PAGE 2 :");
-                        console.log("pages:", pages);
-                        console.log("Correspondance: ", pageId.includes(testExistItem));
                         if(!pageId.includes(testExistItem)){
                             e.target.value = '';
                             window.alert("Le nom du volcan n'a pas été trouvé sur Wikipédia, veuillez vérifier l'orthographe.");
+                            setChecked(false);
                         } 
+                        else {
+                            setChecked(true);
+                            volcanoTypes(e.target.value);
+                        }
                     })
                     .catch((err) => {
                         console.log(err);
                         window.alert("Le nom du volcan n'a pas été trouvé sur Wikipédia, veuillez vérifier l'orthographe.");
                         e.target.value = '';
+                        setChecked(false);
                     })                 
             })
             .catch((err) => {
                 console.log(err);
                 window.alert("Le nom du volcan n'a pas été trouvé sur Wikipédia, veuillez vérifier l'orthographe.");
                 e.target.value = '';
+                setChecked(false);
             });
         
+    }
+    const volcanoTypes = (name) => {
+        console.log('volcanoTypes', name);
+        fetch(`https://fr.wikipedia.org/api/rest_v1/page/summary/${name}`)
+        .then((res) => {
+            if (res.ok){
+                return res.json();
+            }
+        })
+        .then((data) => {
+            console.log("Datas : ", data.extract,data.extract.includes('actif'));
+        })
+        .catch((err) => {
+            console.log(err);
+        })
     }
     const createVolcano = (e) => {
         e.preventDefault();
@@ -122,14 +146,6 @@ function Contribution(){
                 alert("Une erreur est survenue, veuillez réessayer plus tard");
         })
     }     
-    const btnFormat = (e) => {console.log(e.target.value);
-        if (e.target.value === 'DMS'){
-            setFormat('DMS');
-        }
-        else {
-            setFormat('DD')
-        }
-    }
 return(
     <>
         <Banner/>
@@ -143,77 +159,28 @@ return(
                             <div>
                                 <label htmlFor='volcanoName'>Nom du volcan: </label>
                                 <input type = "text" id = "volcanoName" name = 'volcanoName' placeholder = 'Nom' required onBlur = {testingName}></input>
+                                {checked === true ? <span style = {{color: 'green', fontSize: '20px', marginLeft: '10px'}}>&#10003;</span> : null
+                                }
                             </div>
                             <br/>
-                            {/* <div>
-                                <div onChange = {btnFormat}>
-                                    <span>Coordonnées GPS:</span>
-                                    <input type = 'radio' name = 'GPSformat' id = 'DMS' checked value = 'DMS'></input>
-                                    <label htmlFor = 'GPSformat'>DMS</label>
-                                    <input type = 'radio' name = 'GPSformat' id = 'DD' value = 'DD' ></input>
-                                    <label htmlFor = 'GPSformat'>DD</label>
-                                </div>
-                                <div>
-                                    <input type = 'number' id = 'volcanoGPSlat__DMS--D' min = '0' max = '90' style = {{width:'30px'}}></input>
-                                    <span>° </span>
-                                    <input type = 'number' id = 'volcanoGPSlat__DMS--M' min = '0' max = '60' style = {{width:'30px'}}></input>
-                                    <span>' </span>
-                                    <input type = 'number' id = 'volcanoGPSlat__DMS--S' min = '0' max = '60' step = '0.00001' style = {{width:'90px'}}></input>
-                                    <span>" </span>
-                                    <select name = 'lat' id = 'lat'>
-                                        <option value = 'N'>N</option>
-                                        <option value = 'S'>S</option>
-                                    </select>
-                                </div>
-                                <div>
-                                    <input type = 'number' id = 'volcanoGPSlng__DMS--D' min = '0' max = '90' style = {{width:'30px'}}></input>
-                                    <span>° </span>
-                                    <input type = 'number' id = 'volcanoGPSlng__DMS--M' min = '0' max = '60' style = {{width:'30px'}}></input>
-                                    <span>' </span>
-                                    <input type = 'number' id = 'volcanoGPSlng__DMS--S' min = '0' max = '60'  step = '0.00001' style = {{width:'90px'}}></input>
-                                    <span>" </span>
-                                    <select name = 'lng' id = 'lng'>
-                                        <option value = 'O'>O</option>
-                                        <option value = 'E'>E</option>
-                                    </select>
-                                </div>
-                                    : 
-                                    <div>
-                                        <p>Format DD</p>
-                                    </div>
-                            </div> */}
                             <div>
                                 <label htmlFor='constribution__description'>Description: </label><br/>
-                                <textarea type = "text" id = "contribution__description" name = 'contribution__description' required placeholder='Très joli volcan'></textarea><br/><br />
+                                <textarea type = "text" id = "contribution__description" name = 'contribution__description' required placeholder='Partage-nous ton expérience'></textarea><br/><br />
                             </div>
-                            
-                            <div style = {{display: 'flex', flexDirection: 'row', justifyContent: 'start'}}>  
-                                <img id = 'contribution__volcanoTypeImage' src = {typeVolcan} alt = "Type d'éruption"></img>                      
-                                <div style = {{display: 'flex', flexDirection: 'column', justifyContent: 'center'}}>
-                                    <h2>Type de volcan :</h2>
-                                    <br />
-                                    <div>
-                                        <input type = 'radio' id = 'contribution__type1' value = "Explosif"  name = "contribution__volcanoType" required></input>
-                                        <label htmlFor = "contribution__type1" >Explosif</label>
-                                    </div>
-                                    <div>
-                                        <input type = 'radio' id = 'contribution__type2' value = 'Effusif' name = 'contribution__volcanoType'></input>
-                                        <label htmlFor = 'contribution__type2'>Effusif</label>
-                                    </div>
-                                </div>
-                            </div>   
-                           
                         </div>
                         <div id = "contribution__form--imgContainer" >
-                            <label id = 'contribution__imgBtn' htmlFor = 'contribution__volcanoFiles'>Choisir une image</label><br/>
-                            <input type = 'file' onChange = {previewImage} id = 'contribution__volcanoFiles' name = "uploadedFiles" accept = "image/jpeg, image/jpg, image/png" multiple></input><br/>
+                            <div id = "contribution__form--imgContainer--input">
+                                <label id = 'contribution__imgBtn' htmlFor = 'contribution__volcanoFiles'>Choisir une image</label><br/>
+                                <input type = 'file' onChange = {previewImage} onClick = {() => setPictures([])} id = 'contribution__volcanoFiles' name = "uploadedFiles" accept = "image/jpeg, image/jpg, image/png" multiple ></input><br/>
+                                <p>Seules les images horizontales seront ajoutées</p>
+                            </div>
                             <div id = 'contribution__pictures'>
                                 {pictures.map((e,index) =>{
-                                return  <>
+                                    return  <>
                                             <img id = 'contribution__volcanoFilesImage' src = {URL.createObjectURL(e)} alt = {`Volcan n°${index+1}`} key = {`image_${index+1}`} />
-                                        </>
-                            }
-                            )}
+                                            </>
+                                    }
+                                )}
                             </div>
                         </div>
                     </div>
